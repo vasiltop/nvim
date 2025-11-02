0b83a2fd76c059347f46dca647aafb79bb4b545a
vasiltop <vasiltopalovic@outlook.com>
2025-11-02 18:03:30 -0500

    feat: add vue lsp

--- init.lua
+++ init.lua
@@ -82,7 +82,7 @@ if treesitter_ok then
 end
 
 -- lsp
-vim.lsp.enable({ "lua_ls", "basedpyright", "rust_analyzer" })
+vim.lsp.enable({ "lua_ls", "basedpyright", "rust_analyzer", "vtsls", "ts_ls" })
 
 vim.lsp.config("basedpyright", {
 	settings = {
@@ -101,6 +101,38 @@ vim.lsp.config("basedpyright", {
 			}
 		}
 	}
+})
+
+local vue_language_server_path = vim.fn.expand '$MASON/packages' ..
+		'/vue-language-server' .. '/node_modules/@vue/language-server'
+local tsserver_filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' }
+local vue_plugin = {
+	name = '@vue/typescript-plugin',
+	location = vue_language_server_path,
+	languages = { 'vue' },
+	configNamespace = 'typescript',
+}
+
+vim.lsp.config('vtsls', {
+	settings = {
+		vtsls = {
+			tsserver = {
+				globalPlugins = {
+					vue_plugin,
+				},
+			},
+		},
+	},
+	filetypes = tsserver_filetypes,
+})
+
+vim.lsp.config('ts_ls', {
+	init_options = {
+		plugins = {
+			vue_plugin,
+		},
+	},
+	filetypes = tsserver_filetypes,
 })
 
 vim.api.nvim_create_autocmd("LspAttach", {
